4d0adeb342447a70faa3cca49b00193282021aad
heisedebaise <heisedebaise@hotmail.com>
2020-03-24 22:23:57 +0800

    dev

---
+++ clivia-chrome/src/main/java/org/lpw/clivia/chrome/ChromeDao.java
@@ -0,0 +1,16 @@
+package org.lpw.clivia.chrome;
+
+import org.lpw.photon.dao.orm.PageList;
+
+/**
+ * @author lpw
+ */
+interface ChromeDao {
+    PageList<ChromeModel> query(String key, String name, int pageSize, int pageNum);
+
+    ChromeModel findByKey(String key);
+
+    void save(ChromeModel chrome);
+
+    void delete(String id);
+}
---
+++ clivia-chrome/src/main/java/org/lpw/clivia/chrome/ChromeDaoImpl.java
@@ -0,0 +1,50 @@
+package org.lpw.clivia.chrome;
+
+import org.lpw.clivia.util.DaoHelper;
+import org.lpw.photon.dao.orm.PageList;
+import org.lpw.photon.dao.orm.lite.LiteOrm;
+import org.lpw.photon.dao.orm.lite.LiteQuery;
+import org.lpw.photon.util.Validator;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lpw
+ */
+@Repository(ChromeModel.NAME + ".dao")
+class ChromeDaoImpl implements ChromeDao {
+    @Inject
+    private Validator validator;
+    @Inject
+    private LiteOrm liteOrm;
+    @Inject
+    private DaoHelper daoHelper;
+
+    @Override
+    public PageList<ChromeModel> query(String key, String name, int pageSize, int pageNum) {
+        StringBuilder where = new StringBuilder();
+        List<Object> args = new ArrayList<>();
+        daoHelper.like(null, where, args, "c_key", key);
+        daoHelper.like(null, where, args, "c_name", name);
+
+        return liteOrm.query(new LiteQuery(ChromeModel.class).where(where.toString()).size(pageSize).page(pageNum), args.toArray());
+    }
+
+    @Override
+    public ChromeModel findByKey(String key) {
+        return liteOrm.findOne(new LiteQuery(ChromeModel.class).where("c_key=?"), new Object[]{key});
+    }
+
+    @Override
+    public void save(ChromeModel chrome) {
+        liteOrm.save(chrome);
+    }
+
+    @Override
+    public void delete(String id) {
+        liteOrm.deleteById(ChromeModel.class, id);
+    }
+}
