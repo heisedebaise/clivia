90292b1cb6a82c89b5a646cd06f67ca7aba57f61
heisedebaise <heisedebaise@hotmail.com>
2020-04-18 11:13:32 +0800

    dev

--- clivia-console/ui/src/sign-in/index.jsx
+++ clivia-console/ui/src/sign-in/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Form, Input, Button } from 'antd';
+import { Layout, Form, Input, Button, message } from 'antd';
 import { UserOutlined, LockOutlined, GithubOutlined, WechatOutlined, WeiboOutlined, AlipayOutlined } from '@ant-design/icons';
 import { service } from '../http';
 import './index.css';
@@ -16,6 +16,12 @@ class SignIn extends React.Component {
     }
 
     finish = values => {
+        if (values.repeat !== values.password) {
+            message.warn('重复密码与密码必须相同！');
+
+            return;
+        }
+
         values.type = '';
         service('/user/sign-' + (this.state.up ? 'up' : 'in'), values).then(data => {
             if (data != null) {
@@ -37,9 +43,8 @@ class SignIn extends React.Component {
                         <Form onFinish={this.finish}>
                             <Form.Item name="uid"><Input prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" /></Form.Item>
                             <Form.Item name="password"><Input.Password prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="密码" /></Form.Item>
-                            <Form.Item>
-                                <Button type="primary" htmlType="submit" className="sign-in-up-button">{this.state.up ? '提交注册' : '登录'}</Button>
-                            </Form.Item>
+                            {this.state.up ? <Form.Item name="repeat"><Input.Password prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="重复密码" /></Form.Item> : null}
+                            <Form.Item><Button type="primary" htmlType="submit" className="sign-in-up-button">{this.state.up ? '提交注册' : '登录'}</Button></Form.Item>
                             <Form.Item>
                                 <Button type="link" className="sign-in-up-link" onClick={this.change}>{this.state.up ? '使用已有账户登录' : '注册新账户'}</Button>
                                 <span>其他登录方式</span>
