19cae23a981c1b444a59d588009352ba13740577
heisedebaise <heisedebaise@hotmail.com>
2020-10-21 10:19:49 +0800

    dev

--- clivia-user/src/main/java/org/lpw/clivia/user/UserCtrl.java
+++ clivia-user/src/main/java/org/lpw/clivia/user/UserCtrl.java
@@ -77,6 +77,7 @@ public class UserCtrl {
             @Validate(validator = Validators.EMAIL, emptyable = true, parameter = "email", failureCode = 11),
             @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "email", failureCode = 12),
             @Validate(validator = Validators.BETWEEN, number = {0, 2}, parameter = "gender", failureCode = 13),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "portrait", failureCode = 32),
             @Validate(validator = UserService.VALIDATOR_SIGN)
     })
     public Object modify() {
@@ -151,8 +152,39 @@ public class UserCtrl {
                 request.getAsInt("maxGrade", -1), request.getAsInt("state", -1), request.get("register"));
     }
 
+    @Execute(name = "create", validates = {
+            @Validate(validator = Validators.NOT_EMPTY, parameter = "uid", failureCode = 1),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "uid", failureCode = 2),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "idcard", failureCode = 7),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "name", failureCode = 8),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "nick", failureCode = 9),
+            @Validate(validator = Validators.MOBILE, emptyable = true, parameter = "mobile", failureCode = 10),
+            @Validate(validator = Validators.EMAIL, emptyable = true, parameter = "email", failureCode = 11),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "email", failureCode = 12),
+            @Validate(validator = Validators.BETWEEN, number = {0, 2}, parameter = "gender", failureCode = 13),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "portrait", failureCode = 32),
+            @Validate(validator = Validators.BETWEEN, number = {0, 1}, parameter = "state", failureCode = 24),
+            @Validate(validator = AuthService.VALIDATOR_UID_NOT_EXISTS, parameter = "uid", failureCode = 4)
+    })
+    public Object create() {
+        userService.create(request.get("uid"), request.get("password"), request.get("idcard"), request.get("name"), request.get("nick"),
+                request.get("mobile"), request.get("email"), request.get("portrait"), request.getAsInt("gender"), request.getAsSqlDate("birthday"),
+                userService.id(), 0, request.getAsInt("state"));
+
+        return "";
+    }
+
     @Execute(name = "update", validates = {
             @Validate(validator = Validators.ID, parameter = "id", failureCode = 22),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "idcard", failureCode = 7),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "name", failureCode = 8),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "nick", failureCode = 9),
+            @Validate(validator = Validators.MOBILE, emptyable = true, parameter = "mobile", failureCode = 10),
+            @Validate(validator = Validators.EMAIL, emptyable = true, parameter = "email", failureCode = 11),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "email", failureCode = 12),
+            @Validate(validator = Validators.BETWEEN, number = {0, 2}, parameter = "gender", failureCode = 13),
+            @Validate(validator = Validators.MAX_LENGTH, number = {100}, parameter = "portrait", failureCode = 32),
+            @Validate(validator = Validators.BETWEEN, number = {0, 1}, parameter = "state", failureCode = 24),
             @Validate(validator = Validators.SIGN),
             @Validate(validator = UserService.VALIDATOR_EXISTS, parameter = "id", failureCode = 25)
     })
@@ -173,7 +205,7 @@ public class UserCtrl {
 
     @Execute(name = "grade", validates = {
             @Validate(validator = Validators.ID, parameter = "id", failureCode = 22),
-            @Validate(validator = Validators.BETWEEN, number = {0, 99}, parameter = "grade", failureCode = 23),
+            @Validate(validator = Validators.BETWEEN, number = {0, 98}, parameter = "grade", failureCode = 23),
             @Validate(validator = Validators.SIGN),
             @Validate(validator = UserService.VALIDATOR_EXISTS, parameter = "id", failureCode = 25)
     })
