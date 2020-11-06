fd0b26a291a3f51196e916a1fd8a5e6edf835b0f
heisedebaise <heisedebaise@hotmail.com>
2020-11-06 08:54:30 +0800

    dev

---
+++ clivia-web/src/main/webapp/c/precache-manifest.782a1911d145db10083590dfcad7fe21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f52cf0321acdb8e333e662b80fe52dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3cf5b2f9801b7d85d134",
+    "url": "./static/css/2.824c7e15.chunk.css"
+  },
+  {
+    "revision": "f9749ed7284dd83171bb",
+    "url": "./static/css/main.5a3ed92c.chunk.css"
+  },
+  {
+    "revision": "3cf5b2f9801b7d85d134",
+    "url": "./static/js/2.dd88caca.chunk.js"
+  },
+  {
+    "revision": "e2343cdd5d33b87d9fc53b87b61372a7",
+    "url": "./static/js/2.dd88caca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9749ed7284dd83171bb",
+    "url": "./static/js/main.cf8bae4b.chunk.js"
+  },
+  {
+    "revision": "b9792abf4a1da54ea494",
+    "url": "./static/js/runtime-main.16943de7.js"
+  }
+]);
