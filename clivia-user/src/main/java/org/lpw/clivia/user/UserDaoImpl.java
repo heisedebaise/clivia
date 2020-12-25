2d90610656a317f63d07e8b3e84dd7a3130ecd79
heisedebaise <heisedebaise@hotmail.com>
2020-12-25 08:28:08 +0800

    dev

--- clivia-user/src/main/java/org/lpw/clivia/user/UserDaoImpl.java
+++ clivia-user/src/main/java/org/lpw/clivia/user/UserDaoImpl.java
@@ -25,7 +25,8 @@ class UserDaoImpl implements UserDao {
     @Override
     public PageList<UserModel> query(Set<String> ids, String idcard, String name, String nick, String mobile, String email, String weixin, String qq,
                                      String code, int minGrade, int maxGrade, int state, String register, int pageSize, int pageNum) {
-        return daoHelper.newQueryBuilder().where("c_code", DaoOperation.Equals, code)
+        return daoHelper.newQueryBuilder()
+                .where("c_code", DaoOperation.Equals, code)
                 .in("c_id", ids)
                 .where("c_mobile", DaoOperation.Equals, mobile)
                 .where("c_idcard", DaoOperation.Equals, idcard)
