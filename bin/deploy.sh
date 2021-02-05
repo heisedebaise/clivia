8fa9c0dbb413f8244ae4aba253964aba6aeb8920
heisedebaise <heisedebaise@hotmail.com>
2021-02-05 14:03:16 +0800

    dev

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,6 @@ source bin/install.sh
 
 podman stop -t 1 tomcat
 rm -rf ~/tomcat/webapps/ROOT
-cp -r clivia-web/target/clivia-web-0.3 ~/tomcat/webapps/ROOT
+cp -r clivia-web/target/clivia-web-1.0 ~/tomcat/webapps/ROOT
 podman restart tomcat
 tail -f ~/tomcat/logs/catalina.out
