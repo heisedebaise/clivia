c489c93c7aac4de8b5fc3daab3384188bffe8f92
heisedebaise <heisedebaise@hotmail.com>
2021-07-23 17:19:12 +0800

    dev

--- clivia-group/src/main/java/org/lpw/clivia/group/GroupDao.java
+++ clivia-group/src/main/java/org/lpw/clivia/group/GroupDao.java
@@ -1,7 +1,11 @@
 package org.lpw.clivia.group;
 
+import java.util.Set;
+
 import org.lpw.photon.dao.orm.PageList;
 
 interface GroupDao {
     PageList<GroupModel> query(int pageSize, int pageNum);
+
+    PageList<GroupModel> query(Set<String> ids);
 }
--- clivia-group/src/main/java/org/lpw/clivia/group/GroupDaoImpl.java
+++ clivia-group/src/main/java/org/lpw/clivia/group/GroupDaoImpl.java
@@ -1,9 +1,12 @@
 package org.lpw.clivia.group;
 
+import org.lpw.clivia.dao.DaoHelper;
 import org.lpw.photon.dao.orm.PageList;
 import org.lpw.photon.dao.orm.lite.LiteOrm;
 import org.lpw.photon.dao.orm.lite.LiteQuery;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -11,9 +14,16 @@ import javax.inject.Inject;
 class GroupDaoImpl implements GroupDao {
     @Inject
     private LiteOrm liteOrm;
+    @Inject
+    private DaoHelper daoHelper;
 
     @Override
     public PageList<GroupModel> query(int pageSize, int pageNum) {
         return liteOrm.query(new LiteQuery(GroupModel.class).size(pageSize).page(pageNum), null);
+    }
+
+    @Override
+    public PageList<GroupModel> query(Set<String> ids) {
+        return daoHelper.newQueryBuilder().in("c_id", ids).query(GroupModel.class, 0, 0);
     }
 }
