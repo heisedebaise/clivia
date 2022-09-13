77441c44a2b1844e42aa6700dd29e1a2a60c354b
heisedebaise <heisedebaise@hotmail.com>
2022-09-13 15:39:22 +0800

    dev

--- clivia-base/src/main/java/org/lpw/clivia/lock/LockHelperImpl.java
+++ clivia-base/src/main/java/org/lpw/clivia/lock/LockHelperImpl.java
@@ -3,14 +3,21 @@ package org.lpw.clivia.lock;
 import org.lpw.photon.atomic.Atomicable;
 import org.lpw.photon.crypto.Digest;
 import org.lpw.photon.scheduler.SecondsJob;
+import org.lpw.photon.util.Generator;
 import org.lpw.photon.util.Thread;
 import org.lpw.photon.util.TimeUnit;
 import org.lpw.photon.util.Validator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service(LockModel.NAME + ".helper")
 public class LockHelperImpl implements LockHelper, Atomicable, SecondsJob {
@@ -21,8 +28,14 @@ public class LockHelperImpl implements LockHelper, Atomicable, SecondsJob {
     @Inject
     private Validator validator;
     @Inject
+    private Generator generator;
+    @Inject
     private LockDao lockDao;
+    @Value("${clivia.lock.type:0}")
+    private int type;
     private final ThreadLocal<Set<String>> ids = new ThreadLocal<>();
+    private final Map<String, List<LockModel>> map = new ConcurrentHashMap<>();
+    private final Map<String, String> idmd5s = new ConcurrentHashMap<>();
 
     @Override
     public String lock(String key, long wait, int alive) {
@@ -35,11 +48,20 @@ public class LockHelperImpl implements LockHelper, Atomicable, SecondsJob {
         lock.setKey(key);
         lock.setCreate(System.currentTimeMillis());
         lock.setExpire(lock.getCreate() + 1000L * (alive > 0 ? alive : 5));
-        lockDao.save(lock);
+
+        String lockId = type == 1 ? lockDb(md5, lock, wait) : lockMap(md5, lock, wait);
+        if (lockId == null)
+            return null;
 
         if (ids.get() == null)
             ids.set(new HashSet<>());
-        ids.get().add(lock.getId());
+        ids.get().add(lockId);
+
+        return lockId;
+    }
+
+    private String lockDb(String md5, LockModel lock, long wait) {
+        lockDao.save(lock);
         for (long i = 0L; i < wait; i++) {
             LockModel model = lockDao.findByMd5(md5);
             if (model == null)
@@ -50,7 +72,26 @@ public class LockHelperImpl implements LockHelper, Atomicable, SecondsJob {
 
             thread.sleep(1, TimeUnit.MilliSecond);
         }
-        unlock(lock.getId());
+        unlockDb(lock.getId());
+
+        return null;
+    }
+
+    private String lockMap(String md5, LockModel lock, long wait) {
+        lock.setId(generator.uuid());
+        List<LockModel> list = map.computeIfAbsent(md5, key -> Collections.synchronizedList(new ArrayList<>()));
+        list.add(lock);
+        for (long i = 0L; i < wait; i++) {
+            LockModel model = list.get(0);
+            if (model == null)
+                return null;
+
+            if (lock.getId().equals(model.getId()))
+                return lock.getId();
+
+            thread.sleep(1, TimeUnit.MilliSecond);
+        }
+        unlockDb(lock.getId());
 
         return null;
     }
@@ -60,8 +101,31 @@ public class LockHelperImpl implements LockHelper, Atomicable, SecondsJob {
         if (id == null)
             return;
 
+        if (type == 1)
+            unlockDb(id);
+        else
+            unlockMap(id);
+    }
+
+    private void unlockDb(String id) {
         lockDao.delete(id);
         ids.get().remove(id);
+    }
+
+    private void unlockMap(String id) {
+        String md5 = idmd5s.remove(id);
+        if (md5 == null || !map.containsKey(md5))
+            return;
+
+        List<LockModel> list = map.get(md5);
+        for (int i = 0; i < list.size(); i++) {
+            LockModel lock = list.get(i);
+            if (lock.getId().equals(id)) {
+                list.remove(i);
+
+                break;
+            }
+        }
     }
 
     @Override
