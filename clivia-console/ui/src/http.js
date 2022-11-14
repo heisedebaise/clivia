b73ddb1f5c11fd0cad33c18f6556f1051efa85d2
heisedebaise <heisedebaise@hotmail.com>
2022-11-14 08:43:54 +0800

    dev

--- clivia-console/ui/src/http.js
+++ clivia-console/ui/src/http.js
@@ -2,7 +2,7 @@ import {
     message
 } from 'antd';
 
-const root = 'https://beefory.com';
+const root = 'http://localhost:8080';
 
 const service = (uri, body) => post(uri, body).then(json => {
     if (json === null) return null;
