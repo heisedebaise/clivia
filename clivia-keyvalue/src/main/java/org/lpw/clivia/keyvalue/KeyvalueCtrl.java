59cd684c471947f1094df515055c304597fc4645
heisedebaise <heisedebaise@hotmail.com>
2022-12-31 11:54:53 +0800

    dev

--- clivia-keyvalue/src/main/java/org/lpw/clivia/keyvalue/KeyvalueCtrl.java
+++ clivia-keyvalue/src/main/java/org/lpw/clivia/keyvalue/KeyvalueCtrl.java
@@ -2,9 +2,14 @@ package org.lpw.clivia.keyvalue;
 
 import org.lpw.clivia.Permit;
 import org.lpw.photon.ctrl.context.Request;
+import org.lpw.photon.ctrl.context.Response;
 import org.lpw.photon.ctrl.execute.Execute;
+import org.lpw.photon.ctrl.template.Templates;
 import org.lpw.photon.ctrl.validate.Validate;
 import org.lpw.photon.ctrl.validate.Validators;
+import org.lpw.photon.util.Context;
+import org.lpw.photon.util.Io;
+import org.lpw.photon.util.Validator;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -13,7 +18,15 @@ import javax.inject.Inject;
 @Execute(name = "/keyvalue/", key = KeyvalueModel.NAME, code = "101")
 public class KeyvalueCtrl {
     @Inject
+    private Validator validator;
+    @Inject
+    private Context context;
+    @Inject
+    private Io io;
+    @Inject
     private Request request;
+    @Inject
+    private Response response;
     @Inject
     private KeyvalueService keyvalueService;
 
@@ -29,6 +42,20 @@ public class KeyvalueCtrl {
     })
     public Object object() {
         return keyvalueService.object(request.get("key"));
+    }
+
+    @Execute(name = "image", permit = Permit.always, type = Templates.STREAM, validates = {
+            @Validate(validator = Validators.NOT_EMPTY, parameter = "key", failureCode = 2)
+    })
+    public Object image() {
+        String uri = keyvalueService.value(request.get("key"));
+        if (validator.isEmpty(uri))
+            return null;
+
+        String[] contentType = uri.split("/");
+        response.setContentType(contentType[2] + "/" + contentType[3]);
+
+        return io.read(context.getAbsolutePath(uri));
     }
 
     @Execute(name = "save", validates = {
