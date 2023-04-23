fa7019a9733a0467374fb6baab4486d2a9d76573
lpw <lpw@clivia>
2023-04-23 14:28:25 +0800

    dev

--- clivia-group/src/main/java/org/lpw/clivia/group/GroupServiceImpl.java
+++ clivia-group/src/main/java/org/lpw/clivia/group/GroupServiceImpl.java
@@ -16,14 +16,7 @@ import org.lpw.photon.util.Validator;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Service(GroupModel.NAME + ".service")
 public class GroupServiceImpl implements GroupService, UserListener {
